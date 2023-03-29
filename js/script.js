e35d6f5a2b09d012450442a1ac77cebb9b8ac978
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-03-29 17:28:33 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+
+"use strict"
+/**
+ * This function calculates the area of a trapezoid
+ */
+function enterClicked() {
+  //input
+  const ABase = parseInt(document.getElementById("base-a-of-trapezoid").value)
+  const BBase = parseInt(document.getElementById("base-b-of-trapezoid").value)
+  const height = parseInt(document.getElementById("height-of-trapezoid").value
+  )
+
+
+  //process
+  const area = ABase + BBase 
+  const barea = area /2
+  const fullarea = barea * height
+
+
+  //output
+  document.getElementById("area").innerHTML =
+    "The area of the trapezoid is " + fullarea + " cm²."
+}
